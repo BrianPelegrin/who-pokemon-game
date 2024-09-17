cc9d1117df28b5901a169aa05f561806fb5bf557
BrianPelegrin <brianjpdev@gmail.com>
2024-09-17 16:14:15 -0400

    Creating the proyect

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue';
+
+//import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+ <MainLayout />
+</template>
+
+<style scoped>
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <TheNavbar />
+    </header>
+    <main></main>
+    <footer></footer>
+</template>
+
+<script setup lang="ts">
+import TheNavbar from '../components/TheNavbar.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
